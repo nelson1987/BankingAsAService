1cebe0d394c98e66c1ee57871c8b03756a36a429
nelson1987 <nelson.ash@outlook.com>
2023-01-07 17:10:30 -0300

    Inclusao de Dominio

---
+++ Baas.Domain/Account/Create/CreateAccountCommand.cs
@@ -0,0 +1,24 @@
+﻿using MediatR;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Baas.Domain.Account.Create
+{
+    public class CreateAccountCommand : IRequest<CreateAccountResponse>
+    {
+        public int Id { get; set; }
+    }
+    public class CreateAccountResponse
+    {
+    }
+    public class CreateAccountModel
+    {
+    }
+    public class CreateAccountHandler : IRequestHandler<CreateAccountCommand, CreateAccountResponse>
+    {
+        public async Task<CreateAccountResponse> Handle(CreateAccountCommand request, CancellationToken cancellationToken)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
