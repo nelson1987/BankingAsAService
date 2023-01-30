7f70d36289297b99768034fe40bad1ed833993c5
nelson1987 <nelson.ash@outlook.com>
2023-01-30 19:41:20 -0300

    Validators

--- BAAS.Domain/Validators/AberturaCommandValidator.cs
+++ BAAS.Domain/Validators/AberturaCommandValidator.cs
@@ -7,55 +7,13 @@ namespace Baas.Domain.Validators
     {
         public AberturaCommandValidator()
         {
-            //    RuleFor(x => x.Agencia)
-            //        .Matches(@"^[0-9]{4}$")
-            //        .WithMessage("A agência deve conter 4 caracteres numéricos.");
+                RuleFor(x => x.Agencia)
+                    .Matches(@"^[0-9]{4}$")
+                    .WithMessage("A agência deve conter 4 caracteres numéricos.");
             //
             //    RuleFor(x => x.CodigoCompe)
             //        .Matches(@"^[0-9]{3}$")
             //        .WithMessage("O código compe deve conter 3 caracteres numéricos.");
         }
     }
-
-    //public class ContaRepository : IContaRepository
-    //{
-    //    private readonly ApplicationDbContext _context;
-
-    //    public ContaRepository(ApplicationDbContext context)
-    //    {
-    //        _context = context;
-    //    }
-
-    //    public async Task<Conta> AddAsync(Conta entity)
-    //    {
-    //        await _context.Contas.AddAsync(entity);
-    //        await _context.SaveChangesAsync();
-    //        return entity;
-    //    }
-    //}
-    //public class MovimentacaoRepository : IMovimentacaoRepository
-    //{
-    //    private readonly ApplicationDbContext _context;
-
-    //    public MovimentacaoRepository(ApplicationDbContext context)
-    //    {
-    //        _context = context;
-    //    }
-
-    //    public async Task<Movimentacao> AddAsync(Movimentacao entity)
-    //    {
-    //        await _context.Contas.AddAsync(entity);
-    //        await _context.SaveChangesAsync();
-    //        return entity;
-    //    }
-    //}
-
-    //public interface IContaRepository
-    //{
-    //    Task<Conta> AddAsync(Conta entity);
-    //}
-    //public interface IMovimentacaoRepository
-    //{
-    //    Task<Movimentacao> AddAsync(Movimentacao entity);
-    //}
 }
